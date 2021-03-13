0aec61f3d5da1b6b17a4362a87065f8a1cf8d0f1
Ananthu678 <76966844+Ananthu678@users.noreply.github.com>
2021-03-13 13:34:40 +0530

    Update BaseClass.java

--- src/test/java/com/qa/test/BaseClass.java
+++ src/test/java/com/qa/test/BaseClass.java
@@ -17,6 +17,14 @@ public class BaseClass {
 		public void openBrowser() {
 			
 			System.setProperty("webdriver.chrome.driver","/home/ubuntu/chromedriver");
+			
+			ChromeOptions chromeOptions = new ChromeOptions();
+			chromeOptions.addArguments("--headless");
+			chromeOptions.addArguments("--no-sandbox");
+//WebDriver driver = new ChromeDriver(chromeOptions);
+			driver = new ChromeDriver();
+			chromeOptions.addArguments("--headless");
+
 			driver = new ChromeDriver();
 			
 			baseURL="https://www.facebook.com/";
